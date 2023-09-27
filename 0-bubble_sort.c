e7568a752a5736dcb70f03dc04faab02636ba484
Kimotho <kimothoibrahim@yahoo.com>
2023-09-27 03:27:25 +0300

    Add the algorithm code and Big O commentary on the code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,5 +2,21 @@
 
 void bubble_sort(int *array, size_t size)
 {
+    int i;
+    int j;
+    int container;
 
+    for( i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < size - 1; j++)
+        {
+            if(array[i] > array[i + 1])
+            {
+                container = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = container;
+                print_array(array, size);
+                }
+        }
+    }
 }
